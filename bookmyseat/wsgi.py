ec69dc6a3a3daaa5b325ad419e4406524a6011c1
Pranav Khanolkar <pranavkhanolkar589@gmail.com>
2025-06-13 22:29:46 +0530

    first commit

--- bookmyseat/wsgi.py
+++ bookmyseat/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from bookmyseat.wsgi import application  
+
+def handler(request, context):
+    return application(environ=request.environ, start_response=context.start_response)
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
 
