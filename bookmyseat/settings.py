a32717699d627a77658d54d05fb664dba6990059
Pranav Khanolkar <pranavkhanolkar589@gmail.com>
2025-07-14 16:38:03 +0530

    Added WhiteNoise middleware for static file handling on Vercel

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 AUTH_USER_MODEL='auth.User'
