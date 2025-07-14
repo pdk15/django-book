2a1f4805150083d8f428285466762fcfe1e2320a
Pranav Khanolkar <pranavkhanolkar589@gmail.com>
2025-07-14 16:05:03 +0530

    Fix vercel routing: moved wsgi to api/

--- api/wsgi.py
+++ api/wsgi.py
@@ -8,10 +8,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
-
-application = get_wsgi_application()
-app = application
+app = get_wsgi_application()
